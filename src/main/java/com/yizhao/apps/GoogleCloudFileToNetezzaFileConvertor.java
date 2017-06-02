f5b8d88a3b79b31a2316dce08e9b4d32524debda
yzhao <yi.zhao@adara.com>
2017-06-02 16:59:36 -0700

    1

--- src/main/java/com/yizhao/apps/GoogleCloudFileToNetezzaFileConvertor.java
+++ src/main/java/com/yizhao/apps/GoogleCloudFileToNetezzaFileConvertor.java
@@ -8,7 +8,23 @@ import java.util.Scanner;
 
 
 /**
- * {"event_id":1097534875260,"cookie_id":106438361728,"dp_id":2452,"vertical":"hotel","activity_group":"prospecting","activity_type":"search","event_ts":"2016-12-18 14:03:31","hotel_city":"tokyo","page":"ot","location_id":64848}
+ * {
+ * "event_id":1097534875260,
+ * "cookie_id":106438361728,
+ * "dp_id":2452,
+ * "vertical":"hotel",
+ * "activity_group":"prospecting",
+ * "activity_type":"search",
+ * "event_ts":"2016-12-18 14:03:31",
+ * "hotel_city":"tokyo",
+ * "page":"ot",
+ * "location_id":64848
+ * }
+ *
+ * hotel:
+ *      event_id,cookie_id,dp_id,vertical,activity_group,activity_type,event_ts,checkin_date,checkout_date,trip_duration,hotel_name,hotel_brand,hotel_city,hotel_state,hotel_country,number_of_rooms,number_of_travelers,currency_type,avg_daily_rate,hotel_code,booked_date,page,user_id,location_id"
+ * flight:
+ *      event_id,cookie_id,dp_id,vertical,activity_group,activity_type,event_ts,departure_date,return_date,origin_airport,destination_airport,air_carrier,cabin_class,cabin_class_group,currency_type,number_of_travelers,trip_duration,booked_date,airfare,page,user_id,location_id"
  */
 public class GoogleCloudFileToNetezzaFileConvertor {
     public static void main(String[] args){
@@ -39,14 +55,15 @@ public class GoogleCloudFileToNetezzaFileConvertor {
 
     public static void readFile(File f){
         Scanner s = null;
+        String line = null;
         try {
             s = new Scanner(f);
             while (s.hasNextLine()) {
-                String line = s.nextLine();
+                line = s.nextLine();
                 parseJason(line);
             }
         }catch (Exception e){
-
+            System.out.println("failed to parseJason:" + line);
         }finally {
             if (s != null) {
                 s.close();
@@ -55,10 +72,19 @@ public class GoogleCloudFileToNetezzaFileConvertor {
     }
 
 
-    public static void parseJason(String jsonObject){
-        JSONObject obj = new JSONObject(jsonObject);
-       // JSONObject event_id = obj.getString("event_id");
-        System.out.println(obj.get("event_id"));
+    public static void parseJason(String line){
+        JSONObject obj = new JSONObject(line);
+        String event_id = obj.get("event_id").toString();
+        String cookie_id = obj.get("cookie_id").toString();
+        String dp_id = obj.get("dp_id").toString();
+        String vertical = obj.get("vertical").toString();
+        String activity_group = obj.get("activity_group").toString();
+        String activity_type = obj.get("activity_type").toString();
+        String event_ts = obj.get("event_ts").toString();
+        String hotel_city = obj.get("hotel_city").toString();
+        String page = obj.get("page").toString();
+        String location_id = obj.get("location_id").toString();
 
+        String result = event_id + "|" + cookie_id + "|";
     }
 }
