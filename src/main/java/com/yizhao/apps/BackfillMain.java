8890b41abd4f177cf789a2f9d8732c50f6fb7bf5
yzhao <yi.zhao@adara.com>
2017-06-12 10:32:34 -0700

    1

--- src/main/java/com/yizhao/apps/BackfillMain.java
+++ src/main/java/com/yizhao/apps/BackfillMain.java
@@ -21,7 +21,9 @@ import java.net.InetAddress;
  *         <p>
  *         Run it:
  *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar convert /workplace/yzhao/googleFiles/apac/flightFiles/122016 /workplace/yzhao/netezzaFiles/apac/flightFiles/122016 122016 flight
+ *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar convert /workplace/yzhao/googleFiles/apac/flightFiles/012017 /workplace/yzhao/netezzaFiles/apac/flightFiles/012017 012017 flight
  *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar convert /workplace/yzhao/googleFiles/apac/hotelFiles/122016 /workplace/yzhao/netezzaFiles/apac/hotelFiles/122016 122016 hotel
+ *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar convert /workplace/yzhao/googleFiles/apac/hotelFiles/012017 /workplace/yzhao/netezzaFiles/apac/hotelFiles/012017 012017 hotel
  *         <p>
  *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar dump d eng759_backfill_apac 2016-12 2017-03
  *         /usr/java/jdk/bin/java -jar Backfill-jar-with-dependencies.jar dump d eng759_backfill_apac  2016-12
