6b67b6542c9462dc32705a2b3b777893afa6d1b9
yzhao <yi.zhao@adara.com>
2017-06-23 11:58:17 -0700

    1

--- src/main/java/com/yizhao/apps/Scanner/FileFilter/fastrackFileFilter.java
+++ src/main/java/com/yizhao/apps/Scanner/FileFilter/fastrackFileFilter.java
@@ -7,7 +7,7 @@ import java.io.FileFilter;
  * Created by yzhao on 6/13/17.
  */
 public class fastrackFileFilter implements FileFilter {
-    private final String[] okFileExtensions = new String[]{"csv", "force"};
+    private final String[] okFileExtensions = new String[]{".csv", ".csv.force"};
 
     public boolean accept(File file) {
         for (String extension : okFileExtensions) {
